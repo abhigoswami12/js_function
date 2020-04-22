51260cecca8e95a4369f696b0a57dd28a4ff6256
abhigoswami12 <abhishekgoswami157@gmail.com>
2020-04-22 09:39:19 +0530

    add file

--- assignments/expressionVSstatement.md
+++ assignments/expressionVSstatement.md
@@ -9,6 +9,7 @@ function add(var a = 0,var b = 0){
   return a + b;
 }
 add(21, 23);
+// ERROR...Declaration of arguments is not required
 ```
 
 2. What is the output or error of the code below.
@@ -18,6 +19,8 @@ function add(a = 0; b = 0) {
   return a + b;
 }
 add(21, 23);
+// ERROR
+// Instead of semicolon in the argument, there will be ',' sign
 ```
 
 3. What is the output or error of the code below.
@@ -27,6 +30,7 @@ function add(a = 0, b = 0) {
   return a + b;
 }
 add(21, 23);
+// ANS. 44
 ```
 
 4. What is the output or error of the code below.
@@ -36,6 +40,8 @@ function add(a = 0, b) {
   return a + b;
 }
 add(21);
+// Output is NaN as 21 is passed to 'a' but no value is passed to 'b' so 'b' is undefined and undefined + 21 => NaN.
+// However, if we give b = 0 ; and then proceed, output will be '21' as then a = 21 and 'b' will be assigned with '0'.
 ```
 
 5. What is the output or error of the code below.
@@ -45,6 +51,8 @@ function add(a = 0, b = 0) {
   return a + b;
 }
 add(undefined, 21);
+// Output: 21
+// In JavaScript, if a parameter is not passed, then its value become 'undefined' or we can also say that if 'undefined' is passed, then it is treated as if nothing has been passed and hence, the respective parameter takes it's corresponding default value.Here, a = 0; b = 21;
 ```
 
 6. What is the output or error of the code below.
@@ -58,7 +66,11 @@ function knowWhy(value) {
   }
 }
 knowWhy(211);
+
+// Output: ERROR
+// We know that Return and Arguments can only be a value. Here, if() is a statement, not a value(or expression).Hence, the error.
 ```
+
 
 7. What is the output or error of the code below.
 
@@ -71,6 +83,8 @@ function knowWhy(value) {
   }
 }
 knowWhy(21);
+// Output: ERROR
+// Explanation same as earlier.
 ```
 
 8. What is the output or error of the code below.
@@ -80,4 +94,7 @@ function isItIf(ifElse) {
   return ifElse;
 }
 isItIf(if(true){console.log('Testing')});
+
+// Output: ERROR
+// Arguments can only be a value.Here, argument is if() which is a statement not an expression.
 ```
--- assignments/functionTypes.js
+++ assignments/functionTypes.js
@@ -43,11 +43,57 @@ convertToString(21); // "21"
  * @return {number}
  */
 
+//  Function Declaration
+function add(n) {
+  return n + 1;
+}
+
+// function expression
+var add = function (n) {
+  return n + 1;
+}
+
+// arrow function
+var add = n => n+1;
+
+// arrow function with curly bracket
+
+var add = n => {
+  return n+1;
+}
+
+// function invocation
+add(3);
+
+
+
 /**
  * Subtracts one from a given number.
  * @param {number} n
  * @return {number}
  */
+
+ //  Function Declaration
+function sub(n) {
+  return n - 1;
+}
+
+// function expression
+var sub = function (n) {
+  return n - 1;
+}
+
+// arrow function
+var sub = n => n-1;
+
+// arrow function with curly bracket
+
+var sub = n => {
+  return n-1;
+}
+
+// function invocation
+sub(3);
 
 /**
  * Adds two numbers.
@@ -56,12 +102,56 @@ convertToString(21); // "21"
  * @return {number} the sum
  */
 
+ //  Function Declaration
+function add(x, y) {
+  return x + y;
+}
+
+// function expression
+var add = function (x, y) {
+  return x + y;
+}
+
+// arrow function
+var add = (x, y) => x + y;
+
+// arrow function with curly bracket
+
+var add = (x, y) => {
+  return x + y;
+}
+
+// function invocation
+add(2,4);
+
 /**
  * Subtracts the second number from the first.
  * @param {number} x
  * @param {number} y
  * @return {number} the difference
  */
+
+  //  Function Declaration
+function sub(x, y) {
+  return y - x;
+}
+
+// function expression
+var sub = function (x, y) {
+  return y - x;
+}
+
+// arrow function
+var sub = (x, y) => y - x;
+
+// arrow function with curly bracket
+
+var sub = (x, y) => {
+  return y - x;
+}
+
+// function invocation
+sub(2,4);
 
 /**
  * Multiplies two numbers.
@@ -70,6 +160,28 @@ convertToString(21); // "21"
  * @return {number} the product
  */
 
+  //  Function Declaration
+function mul(x, y) {
+  return x * y;
+}
+
+// function expression
+var mul = function (x, y) {
+  return x * y;
+}
+
+// arrow function
+var mul = (x, y) => x * y;
+
+// arrow function with curly bracket
+
+var mul = (x, y) => {
+  return x * y;
+}
+
+// function invocation
+mul(2,4);
+
 /**
  * Divides the first number by the second.
  * @param {number} x
@@ -77,11 +189,55 @@ convertToString(21); // "21"
  * @return {number} the quotient
  */
 
+  //  Function Declaration
+function div(x, y) {
+  return x / y;
+}
+
+// function expression
+var div = function (x, y) {
+  return x / y;
+}
+
+// arrow function
+var div = (x, y) => x / y;
+
+// arrow function with curly bracket
+
+var div = (x, y) => {
+  return x / y;
+}
+
+// function invocation
+div(2,4);
+
 /**
  * Multiplies a number by itself.
  * @param {number} x, number to be squared
  * @return {number} squared
  */
+
+  //  Function Declaration
+function square(x) {
+  return x * x;
+}
+
+// function expression
+var square = function (x) {
+  return x * x;
+}
+
+// arrow function
+var square = (x) => x * x;
+
+// arrow function with curly bracket
+
+var square = (x) => {
+  return x * x;
+}
+
+// function invocation
+square(2);
 
 /**
  * Performs a mathematical operation on two numbers.
@@ -99,12 +255,80 @@ convertToString(21); // "21"
  * @return {boolean} `a` is larger than `b`
  */
 
+   //  Function Declaration
+function greater(a, b) {
+
+  if (a > b){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// function expression
+var greater = function(a, b) {
+  
+  if (a > b){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// arrow function with curly bracket
+
+var greater = (a, b) => {
+  
+  if (a > b){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// function invocation
+greater(2,4);
+
 /**
  * Returns true if `a` is less than `b`.
  * @param {number} a
  * @param {number} b
  * @return {boolean} `a` is smaller than `b`
  */
+
+   //  Function Declaration
+function smaller(a, b) {
+
+  if (a < b){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// function expression
+var smaller = function(a, b) {
+  
+  if (a < b){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// arrow function with curly bracket
+
+var smaller = (a, b) => {
+  
+  if (a < b){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// function invocation
+smaller(2,4);
 
 /**
  * Returns true if `a` and `b` are equal.
@@ -113,12 +337,82 @@ convertToString(21); // "21"
  * @return {boolean} the numbers are equal
  */
 
+   //  Function Declaration
+function equal(a, b) {
+
+  if (a === b){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// function expression
+var equal = function(a, b) {
+  
+  if (a === b){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// arrow function with curly bracket
+
+var equal = (a, b) => {
+  
+  if (a === b){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// function invocation
+equal(4,4);
+
 /**
  * Returns the smallest value of two numbers.
  * @param {number} x
  * @param {number} y
  * @return {number} the smallest number
  */
+
+    //  Function Declaration
+function smallest(a, b) {
+
+  if (a > b){
+    return b;
+  } else {
+    return a;
+  }
+}
+
+// function expression
+var smallest = function(a, b) {
+  
+  if (a > b){
+    return b;
+  } else {
+    return a;
+  }
+}
+
+// arrow function with curly bracket
+
+var smallest= (a, b) => {
+  
+  if (a > b){
+    return b;
+  } else {
+    return a;
+  }
+}
+
+// function invocation
+smallest(2,4);
+
+
 
 /**
  * Returns the largest value of two numbers.
@@ -127,17 +421,109 @@ convertToString(21); // "21"
  * @return {number} the largest number
  */
 
+     //  Function Declaration
+function largest(a, b) {
+
+  if (a > b){
+    return a;
+  } else {
+    return b;
+  }
+}
+
+// function expression
+var largest = function(a, b) {
+  
+  if (a > b){
+    return a;
+  } else {
+    return b;
+  }
+}
+
+// arrow function with curly bracket
+
+var largest = (a, b) => {
+  
+  if (a > b){
+    return a;
+  } else {
+    return b;
+  }
+}
+
+// function invocation
+largest(2,4);
+
+
 /**
  * Returns true if `n` is even.
  * @param {number} n
  * @return {boolean} the number is even
  */
 
+     //  Function Declaration
+function even(n) {
+
+  if (n % 2 == 0) {
+    return true;
+  }
+}
+
+// function expression
+var even = function (n) {
+  
+  if (n % 2 == 0) {
+    return true;
+  }
+}
+
+// arrow function with curly bracket
+
+var even = n => {
+  
+  if (n % 2 == 0) {
+    return true;
+  }
+}
+
+// function invocation
+even(4);
+
+
 /**
  * Returns true if `n` is odd.
  * @param {number} n
  * @return {boolean} the number is odd
  */
+
+//  function declaration
+function odd(n) {
+
+  if (n % 2 != 0) {
+    return true;
+  }
+}
+
+// function expression
+var odd = function (n) {
+  
+  if (n % 2 != 0) {
+    return true;
+  }
+}
+
+// arrow function with curly bracket
+
+var odd = n => {
+  
+  if (n % 2 != 0) {
+    return true;
+  }
+}
+
+// function invocation
+odd(4);
 
 /**
  * Returns a letter grade.
@@ -151,9 +537,98 @@ convertToString(21); // "21"
  * @return {string} the score represented as a letter grade
  */
 
+      //  Function Declaration
+function score(totalMarks, maxMarks) {
+  var per = ( (totalMarks / maxMarks) * 100);
+  per = Math.round(per);
+
+  if (per >= 90 && per <= 100) {
+    grade = "A";
+  } else if (per >= 80 && per <= 89) {
+    grade = "B";
+  } else if (per >= 70 && per <= 79) {
+    grade = "C";
+  } else if (per >= 60 && per <= 69) {
+    grade = "D";
+  } else {
+    grade = "F";
+  }
+  return grade;
+}
+
+// function expression
+var score = function (totalMarks, maxMarks) {
+  var per = ( (totalMarks / maxMarks) * 100);
+  per = Math.round(per);
+  
+  if (per >= 90 && per <= 100) {
+    grade = "A";
+  } else if (per >= 80 && per <= 89) {
+    grade = "B";
+  } else if (per >= 70 && per <= 79) {
+    grade = "C";
+  } else if (per >= 60 && per <= 69) {
+    grade = "D";
+  } else {
+    grade = "F";
+  }
+  return grade;
+}
+
+// arrow function with curly bracket
+
+var score = (totalMarks, maxMarks) => {
+  var per = ( (totalMarks / maxMarks) * 100);
+  per = Math.round(per);
+  
+  if (per >= 90 && per <= 100) {
+    grade = "A";
+  } else if (per >= 80 && per <= 89) {
+    grade = "B";
+  } else if (per >= 70 && per <= 79) {
+    grade = "C";
+  } else if (per >= 60 && per <= 69) {
+    grade = "D";
+  } else {
+    grade = "F";
+  }
+  return grade;
+}
+
+// function invocation
+score(167, 200);
+
+
+
 /**
  * Joins two strings with a space.
  * @param {string} word1
  * @param {string} word2
  * @return {string} joined the words joined with a space
  */
+
+     //  Function Declaration
+     function string(word1, word2) {
+
+      var finalString = word1 + word2;
+      return finalString;
+    }
+    
+    // function expression
+    var string = function (word1, word2) {
+      
+      var finalString = word1 + word2;
+      return finalString;
+    }
+    
+    // arrow function with curly bracket
+    
+    var string = (word1, word2) => {
+      
+      var finalString = word1 + word2;
+      return finalString;
+    }
+    
+    // function invocation
+    string('Abhishek', ' Goswami');
+    
